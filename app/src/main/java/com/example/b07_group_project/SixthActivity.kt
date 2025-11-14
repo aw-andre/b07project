9afea368d00c7c337f3ddcbba4330e2ae5d51124
Suraj <surajacharya637@gmail.com>
2025-11-14 13:52:57 -0500

    feat: Child User Interface

--- app/src/main/java/com/example/b07_group_project/SixthActivity.kt
+++ app/src/main/java/com/example/b07_group_project/SixthActivity.kt
@@ -5,13 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.b07_group_project.R
 
-class MainActivity : AppCompatActivity() {
+class SixthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sixth)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
