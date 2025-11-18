d7ac3c7133d5b392806398791bc3cc64ae451fc1
Suraj <surajacharya637@gmail.com>
2025-11-18 18:10:35 -0500

    fix: changed kotlin code to java and renamed files to be more descriptive.

---
+++ app/src/main/java/com/example/b07_group_project/techniqueHelper.java
@@ -0,0 +1,40 @@
+package com.example.b07_group_project;
+
+import static android.widget.VideoView.*;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class techniqueHelper extends AppCompatActivity {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.techniquehelper);
+
+        ImageButton button = findViewById(R.id.imageButton4);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(techniqueHelper.this, childUserInterfaceHome.class);
+                startActivity(intent);
+            }
+        });
+
+        WebView webView = findViewById(R.id.webView);
+        String video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/Lx_e5nXfi5w?si=qXs3U5z1DgpxBTDY&amp;start=7\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
+        webView.loadData(video, "text/html", "utf-8");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+
+
+    }
+}
---
+++ app/src/main/java/com/example/b07_group_project/troubleBreathing.java
@@ -0,0 +1,25 @@
+package com.example.b07_group_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class troubleBreathing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.troublebreathing);
+        ImageButton button = findViewById(R.id.imageButton2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(troubleBreathing.this, childUserInterfaceHome.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
