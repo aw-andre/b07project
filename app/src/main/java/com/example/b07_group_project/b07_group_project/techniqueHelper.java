047d6613fe40a84d0e9bc15c85778d903d3f8f6c
Suraj <surajacharya637@gmail.com>
2025-11-24 14:40:30 -0500

    feature: embedded a video on how to use the asthma inhaler.

--- app/src/main/java/com/example/b07_group_project/b07_group_project/techniqueHelper.java
+++ app/src/main/java/com/example/b07_group_project/b07_group_project/techniqueHelper.java
@@ -4,11 +4,14 @@ import static android.widget.VideoView.OnClickListener;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.ImageButton;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -31,11 +34,15 @@ public class techniqueHelper extends AppCompatActivity {
             }
         });
 
-        WebView webView = findViewById(R.id.webView);
-        String video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/Lx_e5nXfi5w?si=qXs3U5z1DgpxBTDY&amp;start=7\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
-        webView.loadData(video, "text/html", "utf-8");
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebChromeClient(new WebChromeClient());
+        VideoView videoView = findViewById(R.id.videoView3);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(findViewById(R.id.scrollView2));
+        mediaController.setMediaPlayer(videoView);
+
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.asthma_video));
+        videoView.start();
 
 
     }
